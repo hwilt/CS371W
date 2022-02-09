e85a2c3c5733123ca7d4a9bb84a0fe6eb553aaf3
hwilt <henrywwilt@gmail.com>
2022-02-09 15:17:20 -0500

    Lab 2: Work in class

---
+++ Labs/Lab2/unionfind.py
@@ -0,0 +1,36 @@
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parent = [i for i in range(n)]
+
+    def root(self, i):
+        while i != self.parent[i]:
+            i = self.parent[i]
+        return i
+
+    def find(self, i, j):
+        return self.root(i) == self.root(j)
+
+    def union(self, i, j):
+        root_i = self.root(i)
+        root_j = self.root(j)
+        if root_i != root_j:
+            self.parent[root_j] = root_i
+
+    def __str__(self):
+        return str(self.parent)
+
+def main():
+    x = UnionFind(10)
+    string = "Start:\n" + str(x)
+    print(string)
+    x.union(0,2)
+    x.union(1,8)
+    x.union(7,8)
+    x.union(1,6)
+    x.union(0,1)
+    string = "End:\n" + str(x)
+    print(string)
+
+if __name__ == "__main__":
+    main()
---
+++ Labs/Lab2/unionfindopt.py
@@ -0,0 +1,39 @@
+class UnionFindOpt():
+	def __init__(self, n):
+		self.n = n
+		self._parent = [i for i in range(n)]
+		#self._weights = [1 for i in range(n)]
+
+	def root(self, i):
+		while i != self._parent[i]:
+			i = self._parent[i]
+		return i
+
+	def find(self, i, j):
+		return self.root(i) == self.root(j)
+
+	def union(self, i, j):
+		root_i = self.root(i)
+		root_j = self.root(j)
+		if root_i != root_j: #if not in the same root/ group
+			self._parent[root_j] = root_i
+			#self._weights[root_j] += self._weights[root_i]
+			#self._weights[root_j] = 0
+
+	def __str__(self):
+		return str(self._parent)# + '\n' + str(self._weights)
+
+def main():
+	x = UnionFindOpt(10)
+	string = "Start:\n" + str(x)
+	print(string)
+	x.union(0,2)
+	x.union(1,8)
+	x.union(7,8)
+	x.union(1,6)
+	x.union(0,1)
+	string = "End:\n" + str(x)
+	print(string)
+
+if __name__ == "__main__":
+	main()
