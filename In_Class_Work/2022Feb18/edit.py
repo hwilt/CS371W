b52ec1a35ea4713694dfe9daa0dc6596c6c70371
hwilt <henrywwilt@gmail.com>
2022-02-21 15:22:38 -0500

    In class work: Feb 21

--- In_Class_Work/2022Feb18/edit.py
+++ In_Class_Work/2022Feb18/edit.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+LEFT = 0
+UP = 1
+DIAG = 2
+
 def edit(s1, s2):
     """
     An iterative, dynamic programming version of the string
@@ -33,12 +37,12 @@ def edit(s1, s2):
     for i in range(M+1):
         moves.append([])
         for j in range(N+1):
-            moves[i].append([0])
+            moves[i].append([])
     # Fill in the base cases
-    for j in range(N):
-        moves[0][j] = 1 # Move left if we're at the top row
-    for i in range(M):
-        moves[i][0] = 2 # Move up if we're at the left column
+    for j in range(N+1):
+        moves[0][j] = [0] # Move left if we're at the top row
+    for i in range(M+1):
+        moves[i][0] = [1] # Move up if we're at the left column
     
     # Do the dynamic programming to fill in the table and moves
     for i in range(1, M+1):
@@ -49,21 +53,60 @@ def edit(s1, s2):
             if s1[i-1] != s2[j-1]:
                 cost3 += 1
             table[i][j] = min(cost1, cost2, cost3)
-            moves[i][j] = np.argmin(np.array([cost1, cost2, cost3]))+1
-    cost = int(table[-1, -1])
-
+            if table[i][j] == cost1:
+                moves[i][j].append(LEFT)
+            if table[i][j] == cost2:
+                moves[i][j].append(UP)
+            if table[i][j] == cost3:
+                moves[i][j].append(DIAG)
+    
+    backtrace(moves, [[M,N]])
+    """
     ## TODO: Extract an optimal sequence of moves.
     ## Backtrace from i = M, j = N, following the arrows, until you get to [0, 0]
-    directions = [[0,-1], [-1,0], [-1,-1]]
     i = M
     j = N
     path = []
     while not (i == 0 and j == 0):
-        path.appened([i,j])
-        change = directions[moves[i][j]]
-        i += change[0]
-        j += change[1]
-
+        if moves[i][j] == LEFT:
+            path.append("Adding {} to s1".format(s2[j-1]))
+            j -= 1
+        elif moves[i][j] == UP:
+            path.append("Deleting {} from s1".format(s1[i-1]))
+            i -= 1
+        else:
+            if s1[i-1] != s2[j-1]:
+                path.append("Swapping in {} for {} in s1".format(s2[j-1], s1[i-1]))
+            else:
+                path.append("Matching {}".format(s2[j-1]))
+            i -= 1
+            j -= 1
+    path.reverse()
+    for step in path:
+        print(step)
     return cost
+    """
 
-print("Cost = ", edit("school", "fools"))
+def backtrace(moves, path):
+    [i,j] = path[-1]
+    if i == 0 and j == 0:
+        print(path)
+    else:
+        for move in moves[i][j]:
+            inew = i
+            jnew = j
+            if move == LEFT:
+                jnew -= 1
+            elif move == UP:
+                inew -= 1
+            else:
+                inew -= 1
+                jnew -= 1
+            # add this option
+            path.append([inew, jnew])
+            # recursively explore the rest of the path
+            backtrace(moves, path)
+            # pop off this option and try something else
+            path.pop()
+
+edit("school", "fools")
