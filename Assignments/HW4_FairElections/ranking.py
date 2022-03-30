5f8906d3eea484c59c2660de13a0e1fcbef768f2
Henry Wilt <henrywwilt@gmail.com>
2022-03-30 15:58:44 -0400

    HW4: Working on & Getting nowhere

--- Assignments/HW4_FairElections/ranking.py
+++ Assignments/HW4_FairElections/ranking.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def load_rankings(filename="preferences.csv"):
+def load_permutations(filename="preferences.csv"):
     """
-    Load all student rankings from a file
+    Load all student permutations from a file
 
     Parameters
     ----------
@@ -16,7 +16,7 @@ def load_rankings(filename="preferences.csv"):
     -------
     animals: A list of animals in alphabetical order
     raters: dictionary( 
-        string (Ranker's name): list (This person's ranking as a list of numbers
+        string (Ranker's name): list (This person's permutation as a list of numbers
                                       corresponding to the indices in animals)
     )
     """
@@ -35,6 +35,31 @@ def load_rankings(filename="preferences.csv"):
     return animals, raters
 
 
+def mds(D):
+    """
+    Perform classic multidimensional scaling
+    See notes here:
+    http://www.cs.umd.edu/~djacobs/CMSC828/MDSexplain.pdf
+
+    Parameters
+    ----------
+    D: ndarray(N, N)
+        A matrix of pairwise similarities
+    
+    Return
+    ------
+    Y: ndarray(N, N)
+        MDS projection, with columns in order of variance
+        explained
+    """
+    from numpy import linalg
+    N = D.shape[0]
+    H = np.eye(N) - np.ones((N, N))/N
+    B = -0.5*(H.dot((D*D).dot(H)))
+    U, s, V = linalg.svd(B)
+    Y = np.sqrt(s[None, :])*U
+    return Y
+
 def plot_mds_distances(raters, random_state=0):
     """
     Compute all pairwise Kendall-Tau distances and plot a dimension 
@@ -44,12 +69,11 @@ def plot_mds_distances(raters, random_state=0):
     Parameters
     ----------
     raters: dictionary( 
-        string (Ranker's name): list (This person's ranking as a list of numbers
+        string (Ranker's name): list (This person's permutation as a list of numbers
                                       corresponding to the indices in animals)
     random_state: int
         A seed to determine which random isometry to use for MDS
     """
-    from sklearn.manifold import MDS
     N = len(raters)
     D = np.zeros((N, N))
     rlist = [r for r in raters]
@@ -58,44 +82,68 @@ def plot_mds_distances(raters, random_state=0):
             rater2 = rlist[j]
             D[i, j] = kendall_tau(raters[rater1], raters[rater2])
     D = D+D.T
-    embedding = MDS(n_components=2, dissimilarity='precomputed', random_state=random_state)
-    X = embedding.fit_transform(D)
+    X = mds(D)
     plt.scatter(X[:, 0], X[:, 1])
     for i, r in enumerate(rlist):
         plt.text(X[i, 0], X[i, 1], r)
     plt.title("MDS Projected Kendall-Tau Distances")
 
 
-def kendall_tau(r1, r2):
+def kendall_tau(p1, p2):
     """
     An O(N log N) algorithm for computing the Kendall-Tau Distance
 
     Parameters
     ----------
-    r1: List of N elements
+    p1: List of N elements
         A permutation of the elements 0, 1, 2, ..., N corresponding 
         to the first rating
-    r2: List of N elements
+    p2: List of N elements
         A permutation of the elements 0, 1, 2, .., N corresponding to 
         the second rating
     
     Returns
     -------
-    The Kendall-Tau distance between r1 and r2
+    The Kendall-Tau distance between permutation p1 and p2
     """
-    n = len(r1)
+    n = len(p1)
     discordant_pairs = 0
     for i in range(n):
         for j in range(i+1, n):
-            if r1[i] > r1[j] and r2[i] > r2[j]:
+            if p1[i] > p1[j] and p2[i] > p2[j]:
                 discordant_pairs += 1
-            elif r1[i] < r1[j] and r2[i] < r2[j]:
+            elif p1[i] < p1[j] and p2[i] < p2[j]:
                 discordant_pairs += 1
     return discordant_pairs
 
 
-print(kendall_tau([0, 4, 3, 1, 2], [1, 4, 2, 3, 0]))
-plt.figure(figsize=(8, 8))
-animals, raters = load_rankings()
-plot_mds_distances(raters, 1)
-plt.show()
+def diameter(raters):
+    """
+    Compute the diameter of the raters graph
+
+    Parameters
+    ----------
+    raters: dictionary( 
+        string (Ranker's name): list (This person's permutation as a list of numbers
+                                      corresponding to the indices in animals)
+    """
+    ret = []
+    N = len(raters)
+    D = np.zeros((N, N))
+    rlist = [r for r in raters]
+    for i, rater1 in enumerate(rlist):
+        for j in range(i+1, N):
+            rater2 = rlist[j]
+            D[i, j] = kendall_tau(raters[rater1], raters[rater2])
+
+    D = D+D.T
+    print(np.max(D))
+    return ret
+
+
+#print(kendall_tau([0, 4, 3, 1, 2], [1, 4, 2, 3, 0]))
+plt.figure(figsize=(8,8))
+animals, raters = load_permutations()
+#plot_mds_distances(raters, 1)
+#plt.show()
+print(diameter(raters))
